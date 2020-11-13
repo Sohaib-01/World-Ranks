249cf8978fdaca62f14187b3044b12151313fa3f
Sohaib <55838991+Sohaib-01@users.noreply.github.com>
2020-11-13 13:34:54 +0000

    New updated files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,28 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import CountriesTable from "../components/CountriesTable/CountriesTable";
+import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({ countries }) {
+  console.log(countries);
+  return <Layout>
+    <div className={styles.counts}>Found {countries.length} countries</div>
+  
+  
+    <SearchInput placeholder="Filter by Name, Region or SubRegion" />
 
-      <main className={styles.main}>
-        main
-      </main>
-
-      <footer className={styles.footer}>
-        footer
-      </footer>
-    </div>
-  )
+    <CountriesTable countries={countries} />
+  </Layout>;
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
